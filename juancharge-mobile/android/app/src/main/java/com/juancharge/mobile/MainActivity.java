d2e5d6dabe7dbb10ba2539e52f58fcc5d1311433
Acedo, Mark Relan Gercee <125295018+mawrk19@users.noreply.github.com>
2025-11-07 11:18:57 +0800

    first push

---
+++ juancharge-mobile/android/app/src/main/java/com/juancharge/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.juancharge.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ juancharge-mobile/capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juancharge.mobile',
+  appName: 'JuanChargeMobile',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+  },
+};
+
+export default config;
